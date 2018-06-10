729bb9587a59040e8e579c23750f9681ab13a3f8
Dmcdominic <dmcdominic@gmail.com>
2018-06-10 14:05:37 -0400

    TileEntity and ItemStackHandler progress

--- src/main/java/com/dominic/alliance/blocks/QuadChest.java
+++ src/main/java/com/dominic/alliance/blocks/QuadChest.java
@@ -5,8 +5,14 @@ import com.dominic.alliance.tileentity.TileEntityQuadChest;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
 
 public class QuadChest extends BlockBase implements ITileEntityProvider {
 
@@ -22,6 +28,18 @@ public class QuadChest extends BlockBase implements ITileEntityProvider {
 	@Override
 	public TileEntity createNewTileEntity(World worldIn, int meta) {
 		return new TileEntityQuadChest();
+	}
+	
+	@Override
+	public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+		TileEntityQuadChest te = (TileEntityQuadChest) worldIn.getTileEntity(pos);
+		IItemHandler handler = te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+		for (int slot = 0; slot < handler.getSlots(); slot++) {
+			ItemStack stack = handler.getStackInSlot(slot);
+			worldIn.spawnEntity(new EntityItem(worldIn, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, stack));
+		}
+		
+		super.breakBlock(worldIn, pos, state);
 	}
 
 }
--- src/main/java/com/dominic/alliance/tileentity/TileEntityQuadChest.java
+++ src/main/java/com/dominic/alliance/tileentity/TileEntityQuadChest.java
@@ -1,9 +1,52 @@
 package com.dominic.alliance.tileentity;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 
-public class TileEntityQuadChest extends TileEntity {
+public class TileEntityQuadChest extends TileEntity implements ICapabilityProvider {
 
-	private int crackerCount = 0;
+	private ItemStackHandler handler;
+	private int someCounter;
+	
+	public TileEntityQuadChest() {
+		handler = new ItemStackHandler(16);
+		someCounter = 0;
+	}
+	
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+		super.writeToNBT(compound);
+		compound.setTag("ItemStackHandler", this.handler.serializeNBT());
+		compound.setInteger("someCounter", this.someCounter);
+		return compound;
+	}
 
+	@Override
+	public void readFromNBT(NBTTagCompound compound) {
+		super.readFromNBT(compound);
+		this.handler.deserializeNBT(compound.getCompoundTag("ItemStackHandler"));
+		this.someCounter = compound.getInteger("someCounter");
+	}
+	
+	@Override
+	public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
+		if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+			return (T) this.handler;
+		}
+		return super.getCapability(capability, facing);
+	}
+	
+	@Override
+	public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
+		if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+			return true;
+		}
+		return super.hasCapability(capability, facing);
+	}
+	
 }
